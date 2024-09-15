8d60406ae662e76699582a4897da5fce0567370b
shsobur <shsoburhossen951@gmail.com>
2024-09-15 18:50:44 +0600

    Home page baner re-design completed

--- src/Layout/Pages/HomePage/HomeBaner/HomeBaner.jsx
+++ src/Layout/Pages/HomePage/HomeBaner/HomeBaner.jsx
@@ -1,5 +1,17 @@
 import "./HomeBaner.css";
 
+// Baner image__
+
+import banerImg1 from "../../../../assets/banerimg/baner-1.png";
+import banerImg2 from "../../../../assets/banerimg/baner-2.png";
+import banerImg3 from "../../../../assets/banerimg/baner-3.png";
+import banerImg4 from "../../../../assets/banerimg/baner-4.png";
+import banerImg5 from "../../../../assets/banerimg/baner-5.png";
+// Swper js__
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
 const HomeBaner = () => {
   return (
     <>
@@ -7,8 +19,38 @@ const HomeBaner = () => {
         <div className="main_baner_outer_container">
           <div className="main_baner_inner_container">
 
-            <div className="main_baner_content_container">
+            <div className="baner_bg_image_main_container">
+              <Swiper
+                loop={true}
+                slidesPerView={1}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                className="mySwiper swiper_container"
+              >
+                <SwiperSlide>
+                  <img src={banerImg1} alt="img" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={banerImg2} alt="img" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={banerImg3} alt="img" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={banerImg4} alt="img" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={banerImg5} alt="img" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
 
+            <div className="baner_bg_overlay"></div>
+
+            <div className="main_baner_content_container">
               {/* __Baner right side content__ */}
               <div className="baner_left_contect_container">
                 <h1>Paradise View</h1>
@@ -19,12 +61,37 @@ const HomeBaner = () => {
 
               {/* __Baner left side content__ */}
               <div className="baner_right_contect_container">
-                <img src="https://i.ibb.co.com/6DXQcjj/sasha-kaunas-x-Ea-Aoiz-NFV8-unsplash.jpg" />
+                <Swiper
+                  loop={true}
+                  slidesPerView={1}
+                  autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                  }}
+                  modules={[Autoplay]}
+                  className="mySwiper swiper_mini_container"
+                >
+                  <SwiperSlide>
+                    <img src={banerImg1} alt="img" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={banerImg2} alt="img" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={banerImg3} alt="img" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={banerImg4} alt="img" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src={banerImg5} alt="img" />
+                  </SwiperSlide>
+                </Swiper>
               </div>
-
             </div>
 
           </div>
+
         </div>
       </div>
     </>
