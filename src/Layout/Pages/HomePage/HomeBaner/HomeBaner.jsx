a2855a552e25fa554ddb0f31c8760f7189f8927b
shsobur <shsoburhossen951@gmail.com>
2024-09-15 16:30:01 +0600

    Home page baner design completed

--- src/Layout/Pages/HomePage/HomeBaner/HomeBaner.jsx
+++ src/Layout/Pages/HomePage/HomeBaner/HomeBaner.jsx
@@ -2,9 +2,32 @@ import "./HomeBaner.css";
 
 const HomeBaner = () => {
   return (
-    <div>
-      <h2>Hello baner</h2>
-    </div>
+    <>
+      <div className="main_container">
+        <div className="main_baner_outer_container">
+          <div className="main_baner_inner_container">
+
+            <div className="main_baner_content_container">
+
+              {/* __Baner right side content__ */}
+              <div className="baner_left_contect_container">
+                <h1>Paradise View</h1>
+                <h2>Hotel for every moment rich in emotion</h2>
+                <p>Every moment feels like the first time in paradise view</p>
+                <button>Book Now</button>
+              </div>
+
+              {/* __Baner left side content__ */}
+              <div className="baner_right_contect_container">
+                <img src="https://i.ibb.co.com/6DXQcjj/sasha-kaunas-x-Ea-Aoiz-NFV8-unsplash.jpg" />
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
