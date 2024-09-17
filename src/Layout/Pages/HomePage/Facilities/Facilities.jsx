3d28b16ab4dbd025bf09f908b4a3add9fd51ca63
shsobur <shsoburhossen951@gmail.com>
2024-09-17 10:55:06 +0600

    Facilities section design completed

--- src/Layout/Pages/HomePage/Facilities/Facilities.jsx
+++ src/Layout/Pages/HomePage/Facilities/Facilities.jsx
@@ -1,9 +1,10 @@
-import { FaWifi } from "react-icons/fa";
+import { FaDumbbell, FaWifi } from "react-icons/fa";
 import "./Facilities.css";
 import { BiSolidParking } from "react-icons/bi";
 import { MdOutlineLocalLaundryService } from "react-icons/md";
 import { GoLightBulb } from "react-icons/go";
-import { IoFastFood } from "react-icons/io5";
+import { IoFastFood, IoGameController } from "react-icons/io5";
+import { FaPersonSwimming } from "react-icons/fa6";
 
 const Facilities = () => {
   return (
@@ -13,7 +14,6 @@ const Facilities = () => {
           <div className="main_facilities_inner_container">
 
             <div className="main_facilities_content_container">
-
               <div
                 // data-aos="zoom-in"
                 // data-aos-easing="linear"
@@ -27,56 +27,107 @@ const Facilities = () => {
               </div>
 
               <div className="facilities_main_section">
-
+                
                 <div className="facilities_section_item_container">
                   <div className="facilities_section_item_title">
                     <h2>Regular</h2>
                   </div>
 
                   <div className="main_facilities_name_container">
-
-                    <div id="facilities_box_1" className="facilities_name_container">
+                    <div className="facilities_name_container">
                       <h2>
                         <FaWifi />
                       </h2>
                       <p>Wi-Fi</p>
                     </div>
 
-                    <div id="facilities_box_2" className="facilities_name_container">
+                    <div className="facilities_name_container">
                       <h2>
                         <BiSolidParking />
                       </h2>
                       <p>Parking space</p>
                     </div>
 
-                    <div id="facilities_box_3" className="facilities_name_container">
+                    <div className="facilities_name_container">
                       <h2>
                         <MdOutlineLocalLaundryService />
                       </h2>
                       <p>Laundry</p>
                     </div>
 
-                    <div id="facilities_box_4" className="facilities_name_container">
+                    <div className="facilities_name_container">
                       <h2>
                         <GoLightBulb />
                       </h2>
                       <p>24/7 Light</p>
                     </div>
 
-                    <div id="facilities_box_5" className="facilities_name_container">
+                    <div className="facilities_name_container">
                       <h2>
                         <IoFastFood />
                       </h2>
                       <p>Breakfast</p>
                     </div>
-
                   </div>
-
                 </div>
 
                 <div className="facilities_section_item_container">
                   <div className="facilities_section_item_title">
                     <h2>Suite</h2>
+                  </div>
+
+                  <div
+                    id="facilities_center_container"
+                    className="main_facilities_name_container"
+                  >
+                    <div className="facilities_name_container">
+                      <h2>
+                        <FaDumbbell />
+                      </h2>
+                      <p>*Gym</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <FaPersonSwimming />
+                      </h2>
+                      <p>*Swimming Pool</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <FaWifi />
+                      </h2>
+                      <p>Wi-Fi</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <BiSolidParking />
+                      </h2>
+                      <p>Parking space</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <MdOutlineLocalLaundryService />
+                      </h2>
+                      <p>Laundry</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <GoLightBulb />
+                      </h2>
+                      <p>24/7 Light</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <IoFastFood />
+                      </h2>
+                      <p>Breakfast</p>
+                    </div>
                   </div>
                 </div>
 
@@ -84,10 +135,67 @@ const Facilities = () => {
                   <div className="facilities_section_item_title">
                     <h2>VIP</h2>
                   </div>
+
+                  <div className="main_facilities_name_container">
+                    <div className="facilities_name_container">
+                      <h2>
+                        <IoGameController />
+                      </h2>
+                      <p>**Game center</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <FaDumbbell />
+                      </h2>
+                      <p>*Gym</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <FaPersonSwimming />
+                      </h2>
+                      <p>*Swimming Pool</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <FaWifi />
+                      </h2>
+                      <p>Wi-Fi</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <BiSolidParking />
+                      </h2>
+                      <p>Parking space</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <MdOutlineLocalLaundryService />
+                      </h2>
+                      <p>Laundry</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <GoLightBulb />
+                      </h2>
+                      <p>24/7 Light</p>
+                    </div>
+
+                    <div className="facilities_name_container">
+                      <h2>
+                        <IoFastFood />
+                      </h2>
+                      <p>Breakfast</p>
+                    </div>
+                  </div>
                 </div>
 
               </div>
-
             </div>
 
           </div>
