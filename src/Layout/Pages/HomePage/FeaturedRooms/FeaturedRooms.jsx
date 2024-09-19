0cbf9e87aa2a1eeae0cf8c8ef269fcd221194dd6
shsobur <shsoburhossen951@gmail.com>
2024-09-19 20:39:23 +0600

    Added animation on facilities section

--- src/Layout/Pages/HomePage/FeaturedRooms/FeaturedRooms.jsx
+++ src/Layout/Pages/HomePage/FeaturedRooms/FeaturedRooms.jsx
@@ -9,27 +9,52 @@ const FeaturedRooms = () => {
 
             <div className="featured_title_container">
               <h2>Featured Rooms</h2>
-              <p>Find your fearured room with your match. We provide 99.9% quality.</p>
+              <p>
+                Find your fearured room with your match. We provide 99.9%
+                quality.
+              </p>
             </div>
 
             <div className="featured_main_content_container">
-
-              <div id="fectured_image_section_1" className="featured_image_container">
+              <div
+                data-aos="fade-right"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+                id="fectured_image_section_1"
+                className="featured_image_container"
+              >
                 <h2>All</h2>
               </div>
 
-              <div id="fectured_image_section_2" className="featured_image_container">
+              <div
+                data-aos="fade-right"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+                id="fectured_image_section_2"
+                className="featured_image_container"
+              >
                 <h2>Regular</h2>
               </div>
 
-              <div id="fectured_image_section_3" className="featured_image_container">
+              <div
+                data-aos="fade-right"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+                id="fectured_image_section_3"
+                className="featured_image_container"
+              >
                 <h2>Suit</h2>
               </div>
 
-              <div id="fectured_image_section_4" className="featured_image_container">
+              <div
+                data-aos="fade-right"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+                id="fectured_image_section_4"
+                className="featured_image_container"
+              >
                 <h2>VIP</h2>
               </div>
-
             </div>
 
             <div className="featured_btn_container">
